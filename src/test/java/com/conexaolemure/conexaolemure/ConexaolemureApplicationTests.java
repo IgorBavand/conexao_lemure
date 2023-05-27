f3b93bfbe74340b672bd6cd464e497aaf7d18325
igorbavand <igor.bvn@gmail.com>
2023-05-27 17:34:30 -0300

    Initial commit

---
+++ src/test/java/com/conexaolemure/conexaolemure/ConexaolemureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conexaolemure.conexaolemure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexaolemureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
